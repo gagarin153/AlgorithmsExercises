b2a89d97407852f590caaf5f0a326342f0074dde
Sultan <Sultan@Sultan.lan>
2018-03-20 22:01:00 +0300

    Exrecises

--- 1.1.11/src/com/company/Main.java
+++ 1.1.11/src/com/company/Main.java
@@ -1,20 +1,21 @@
 package com.company;
 
 import edu.princeton.cs.introcs.StdOut;
+//Write code fragment to print the contents of two-dimensional boolean array, using * to represent  true a a space to represent false
 
 public class Main {
 
     public static void main(String[] args) {
 
-        boolean[][] matrix = { { false, true, false}, { true, false, true } };
+        boolean[][] matrix = {{false, true, false}, {true, false, true}};
 
-        for(int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++)
                 if (matrix[i][j] == true)
                     StdOut.print("*");
                 else
                     StdOut.print(" ");
-                StdOut.println();
+            StdOut.println();
         }
 
     }
--- 1.1.13/src/com/company/Main.java
+++ 1.1.13/src/com/company/Main.java
@@ -2,18 +2,19 @@ package com.company;
 
 public class Main {
 
-    //write a static method lg() that takes an int value n as argument and returns the largest int not larger than the base 2 logarithm of n. Do n0t use Math
-    public  static int lg(int n) {
+    //write a static method lg() that takes an int value n as argument and returns the largest int not larger than the base 2 logarithm of n. Do not use Math
+    public static int lg(int n) {
         int pow = 0;
-        while(n > 1) {
+        while (n > 1) {
             n /= 2;
             pow++;
         }
         return pow;
     }
+
     public static void main(String[] args) {
 
-            System.out.print(lg(1));
-        }
+        System.out.print(lg(1));
+    }
 }
 
